05684f3f07f9a8918be19b11a9a8f7a65508327a
milo ma <milobluebell@gmail.com>
2020-03-13 01:02:58 +0800

    feat: add get getNumFromRatio function

--- src/demos/home/home.tsx
+++ src/demos/home/home.tsx
@@ -6,7 +6,8 @@ import countdown from 'src/utils/Time/countdown';
 import countdownFromNow from 'src/utils/Time/countdown/fromNow';
 import countdownFromDuration from 'src/utils/Time/countdown/fromDuration';
 import getParamsFromUrl from 'src/utils/Url/getParamsFromUrl';
-import translateIntoRatio from 'src/utils/Number/getRatioFromNum';
+import getRatioFromNum from 'src/utils/Number/getRatioFromNum';
+import getNumFromRatio from 'src/utils/Number/getNumFromRatio';
 
 const hStyle = {
   backgroundColor: `#8e8e8e`,
@@ -22,7 +23,8 @@ const Home: FunctionalComponent = () => {
       <Demo params={[2398348800]} method={countdownFromNow}></Demo>
       <Demo params={[6124, '{d}天{h}小时{m}分钟{s}秒']} method={countdownFromDuration}></Demo>
       <Demo params={[['test', 'test2'], 'http://192.168.102.11:8080/?test=1234&test2=51']} method={getParamsFromUrl}></Demo>
-      <Demo params={[0.24242645, (Math.random() * 2).toFixed(0), true]} method={translateIntoRatio}></Demo>
+      <Demo params={[0.24242645, (Math.random() * 2).toFixed(0), true]} method={getRatioFromNum}></Demo>
+      <Demo params={['23.3452%', (Math.random() * 2).toFixed(0), false]} method={getNumFromRatio}></Demo>
     </div>
   );
 };
--- src/utils/Number/getNumFromRatio/index.ts
+++ src/utils/Number/getNumFromRatio/index.ts
@@ -6,8 +6,13 @@ import { supplyZero } from '../aux';
  * @func 将传入的百分比值，转换为number
  * @param $ratio      string*  将要被转换的数字，通常为小数
  * @param $precision  number*  精度(小数点后位数)
+ * @return            number
  */
-const getNumFromRatio = ($ratio: string, $precision?: number) => {
-
+const getNumFromRatio = ($ratio: string, $precision?: number, $supplemental?: boolean) => {
+  const theNum = parseFloat($ratio.split('%')[0]);
+  const precision = $precision || $ratio.length;
+  const result = round(theNum * 100, precision);
+  console.log(theNum);
+  return $supplemental ? supplyZero(result, precision) : result;
 }
 export default getNumFromRatio;
