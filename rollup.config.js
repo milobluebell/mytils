c1663eecb337183f7b4e0adf730bccfebb488026
milo ma <milobluebell@gmail.com>
2020-03-18 15:14:35 +0800

    chore(dist): reconfig dist scripts

--- rollup.config.js
+++ rollup.config.js
@@ -4,62 +4,71 @@ import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 const path = require('path');
-const fs = require('fs');
 
 const packageJson = require('./package.json');
 const srcDir = 'src';
 
-const basicConfig = function (type = 'es') {
+const emitConfig = (type) => {
   return {
     input: `${path.resolve(srcDir)}/utils/index.ts`,
-    output: Object.assign({}, {
-      indent: false,
-      format: type,
-      sourceMap: true,
-      exports: 'named',
-      globals: {
-        'moment': 'moment',
-        '_': 'lodash-es',
+    output: Object.assign(
+      {},
+      {
+        indent: false,
+        format: type,
+        sourceMap: true,
+        exports: 'named',
+        globals: {
+          moment: 'moment',
+          'lodash-es': 'lodashEs',
+        },
       },
-    }, type === 'es' ? { dir: packageJson.module, } :
-      type === 'cjs' ? { file: packageJson.main, } :
-        {
-          file: packageJson.unpkg,
-          name: packageJson.name,
-        }),
+      type === 'umd'
+        ? {
+            file: packageJson.unpkg,
+            name: packageJson.name,
+          }
+        : {
+            dir: type === 'es' ? 'es' : 'dist/lib',
+          },
+    ),
     external: Object.keys(packageJson.peerDependencies || {}),
     plugins: [
       nodeResolve({
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js'],
       }),
       json(),
       typescript({
         useTsconfigDeclarationDir: true,
         compilerOptions: {
-          declaration: type === 'umd' ? false : true
-        }
+          declaration: type === 'umd' ? false : true,
+        },
       }),
       replace({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      })
-    ].concat(type === 'umd' ? [
-      terser({
-        compress: {
-          pure_getters: true,
-          unsafe: true,
-          unsafe_comps: true,
-          warnings: false
-        }
-      })] : []),
-  }
-}
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+    ].concat(
+      type === 'umd'
+        ? [
+            terser({
+              compress: {
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                warnings: false,
+              },
+            }),
+          ]
+        : [],
+    ),
+  };
+};
 
 //
-export default ['es', 'cjs', 'umd'].map(item => {
-  const config = basicConfig(item);
-  if (item === 'es') {
+export default ['es', 'cjs', 'umd'].map((item) => {
+  const config = emitConfig(item);
+  if (item === 'es' || item === 'cjs') {
     config.input = require('./modules.js');
-    // config.output['entryFileNames'] = `[name].js`;
   }
   return config;
 });
