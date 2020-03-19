ddf769e88280d4a52ed8ec96732b029c10b9f0d9
milobluebell <milobluebell@gmail.com>
2020-03-20 01:31:34 +0800

    feat: 添加countdown和countdownFromDuration的unit test

---
+++ src/utils/Time/countdown/__tests__/index.ts
@@ -0,0 +1,76 @@
+import countdown from '../index';
+import { stringFormatter, objFormatter } from './../fromDuration/__tests__';
+
+describe('countdown', function() {
+  const normalTestDesc = `calculated gutter was smaller than`;
+  const testers = [
+    {
+      moreDesc: `1 hour`,
+      startAt: 1577812953,
+      endAt: 1577812953 + 130,
+      toBe: `02:10`,
+    },
+    {
+      moreDesc: `24 hour`,
+      startAt: 1577812953,
+      endAt: 1577812953 + 60 * 60 * 12,
+      toBe: `12:00:00`,
+    },
+    {
+      moreDesc: `72 hour`,
+      duration: 3 * 24 * 60 * 60 - 40,
+      startAt: 1577812953,
+      endAt: 1577812953 + 60 * 60 * 40,
+      toBe: `1:16:00:00`,
+    },
+    {
+      moreDesc: `1 hour with configured string formatter`,
+      startAt: 1577812953,
+      endAt: 1577812953 + 60 * 2 + 10,
+      formatter: stringFormatter,
+      toBe: `0:00:02:10`,
+    },
+    {
+      moreDesc: `24 hour with configured string formatter`,
+      startAt: 1577812953,
+      endAt: 1577812953 + 60 * 60 * 12 + 10,
+      formatter: stringFormatter,
+      toBe: `0:12:00:10`,
+    },
+    {
+      moreDesc: `72 hour with configured string formatter`,
+      startAt: 1577812953,
+      endAt: 1577812953 + 60 * 60 * 25 + 10,
+      formatter: stringFormatter,
+      toBe: `0:25:00:10`,
+    },
+    {
+      moreDesc: `1 min with configured object formatter`,
+      startAt: 1577812953,
+      endAt: 1577812953 + 40,
+      formatter: objFormatter,
+      toBe: `40`,
+    },
+    {
+      moreDesc: `2 min with configured object formatter`,
+      startAt: 1577812953,
+      endAt: 1577812953 + 90,
+      formatter: objFormatter,
+      toBe: `1:30`,
+    },
+    {
+      moreDesc: `3 min with configured object formatter`,
+      startAt: 1577812953,
+      endAt: 1577812953 + 150,
+      formatter: objFormatter,
+      toBe: `03:30`,
+    },
+  ];
+
+  testers.forEach((rule: any) => {
+    it(`${normalTestDesc} ${rule.moreDesc}`, function() {
+      const duration = rule.duration;
+      expect(countdown(rule.startAt, rule.endAt, rule?.formatter)).toBe(rule.toBe);
+    });
+  });
+});
--- src/utils/Time/countdown/fromDuration/__tests__/index.ts
+++ src/utils/Time/countdown/fromDuration/__tests__/index.ts
@@ -1,16 +1,16 @@
 import countdownFromDuration from '../index';
 
+export const stringFormatter = `{d}:{hh}:{mm}:{ss}`;
+export const objFormatter = {
+  // 1min以内
+  [`${1 * 60}s`]: '{ss}',
+  // 2min以内
+  [`${2 * 60}s`]: '{m}:{s}',
+  // 3min以内
+  [`${3 * 60}s`]: '{mm}:{ss}',
+};
 describe('countdownFromDuration', function() {
   const normalTestDesc = `duration was smaller than`;
-  const stringFormatter = `{d}:{hh}:{mm}:{ss}`;
-  const objFormatter = {
-    // 1min以内
-    [`${1 * 60}s`]: '{ss}',
-    // 2min以内
-    [`${2 * 60}s`]: '{m}:{s}',
-    // 3min以内
-    [`${3 * 60}s`]: '{mm}:{ss}',
-  };
   const testers = [
     {
       moreDesc: `1 hour`,
@@ -20,7 +20,7 @@ describe('countdownFromDuration', function() {
     {
       moreDesc: `24 hour`,
       duration: 24 * 30 * 60 - 30,
-      toBe: `11:00:30`,
+      toBe: `11:59:30`,
     },
     {
       moreDesc: `72 hour`,
@@ -31,7 +31,7 @@ describe('countdownFromDuration', function() {
       moreDesc: `1 hour with configured string formatter`,
       duration: 25 * 60 - 30,
       formatter: stringFormatter,
-      toBe: `0:00:25:30`,
+      toBe: `0:00:24:30`,
     },
     {
       moreDesc: `24 hour with configured string formatter`,
@@ -46,10 +46,22 @@ describe('countdownFromDuration', function() {
       toBe: `2:23:59:20`,
     },
     {
-      moreDesc: `72 hour with configured string formatter`,
-      duration: 1 * 60,
+      moreDesc: `1 min with configured object formatter`,
+      duration: 1 * 60 - 1,
       formatter: objFormatter,
-      toBe: ``,
+      toBe: `59`,
+    },
+    {
+      moreDesc: `2 min with configured object formatter`,
+      duration: 2 * 60 - 1,
+      formatter: objFormatter,
+      toBe: `1:59`,
+    },
+    {
+      moreDesc: `3 min with configured object formatter`,
+      duration: 3 * 60 - 1,
+      formatter: objFormatter,
+      toBe: `02:59`,
     },
   ];
 
